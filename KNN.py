66e5659e92cd1c061f0ce48dff91bc588f9495ff
liuyang <liuyang02@b.360.cn>
2016-12-22 20:05:10 +0800

    continue

--- KNN.py
+++ KNN.py
@@ -10,35 +10,36 @@ def createDataSet():
     labels = ['A', 'A', 'B', 'B']
     return group, labels
 
+
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
     sqDiffMat = diffMat ** 2
     sqDistances = sqDiffMat.sum(axis = 1)
     distances = sqDistances ** 0.5
-    sortedDistIndicies = distances.argsort()
+    sortedDistIndicies = distances.argsort() 
     classCount = {}
+
     for i in range (k):
         voteIlabel = labels[sortedDistIndicies[i]];
         classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+
     sortedClassCount = sorted(classCount.iteritems(),
                               key = operator.itemgetter(1),
                               reverse = True)
+
     return sortedClassCount[0][0]
 
 
 dataSet, labels = createDataSet()
 
-data = array([-1., -1.])
-diffMat = tile(data, (dataSet.shape[0], 1)) - dataSet
-sqDiffMat = diffMat ** 2
-sqDistances = sqDiffMat.sum(axis = 1)
-distances = sqDistances ** 0.5
-sortedDistIndicies = distances.argsort()
-print sortedDistIndicies
+print dataSet.ndim, dataSet.itemsize
+#data = array([-1., -1.])
+#diffMat = tile(data, (dataSet.shape[0], 1)) - dataSet
+#sqDiffMat = diffMat ** 2
+#sqDistances = sqDiffMat.sum(axis = 1)
+#distances = sqDistances ** 0.5
+#sortedDistIndicies = distances.argsort()
+#print sortedDistIndicies
 
-print classify0(data, dataSet, labels, 3);
-#//test1 = tile(array([9., 9.]), (dataSet.shape[0], 1)) - dataSet
-#//print test - test1
-#print test
-#print test1
+#print classify0(data, dataSet, labels, 3);
---
+++ logistic.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+from numpy import *
+
+def loadDataSet():
+    dataMat = []; labelMat = []
+    fr = open('testSet.txt')
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
+        labelMat.append([int(lineArr[2])])
+    return dataMat, labelMat
+
+dataMat, labelMat = loadDataSet()
+print dataMat, '\n'
+print labelMat,
